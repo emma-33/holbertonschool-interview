e2a8fc75a5d58d8b7cd7a19071816a4a04edfe51
Emma Lateyron <emma.lateyron@orange.fr>
2025-04-04 13:23:12 +0200

    [FIX] 0-lockboxes.py // check next element if already in list

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,17 +4,22 @@
 
 def canUnlockAll(boxes):
     """Determine if all boxes can be opened."""
-    newList = []
-    compareList = []
+    newList = [0]
+    compareList = [0]
 
     for i in range(1, len(boxes)):
         compareList.append(i)
 
     for row in boxes:
-        for element in row[:1]:
+        for element in row:
             if element not in newList and element != 0:
                 newList.append(element)
+                break
+            elif element in newList:
+                continue
+
         newList.sort()
+
 
     if newList == compareList:
         return True
