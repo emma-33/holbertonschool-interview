16184c15739a9063ee81c474e7458808bada849d
Emma Lateyron <emma.lateyron@orange.fr>
2025-04-25 15:32:33 +0200

    [ADD] 0-minoperations.py

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to determine the minimum number of operations
+to reach a given number of characters in a text file.
+"""
+
+def minOperations(n):
+    """Determine the minimum number of operations to reach n characters."""
+
+    if n <= 1:
+        return 0
+    
+    min_operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            min_operations += factor
+            n //= factor
+        factor += 1
+
+    return min_operations
+
