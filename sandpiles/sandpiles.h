c7326bcf1fe2ae232f33e5c745752656abf56abb
Emma Lateyron <emma.lateyron@orange.fr>
2025-05-02 14:20:12 +0200

    [ADD] README.md and header file

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+int helper_verify(int grid[3][3]);
+
+#endif/* SANDPILES_H */
