a1480a7df2ed5970abaf5c949fde4e578288b9c7
Emma Lateyron <emma.lateyron@orange.fr>
2025-05-17 17:41:41 +0200

    [FIX] 0-stats.py // pep8 and split()

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Script that reads stdin and computes HTTP metrics
 """
 
@@ -7,43 +7,37 @@ import sys
 import re
 from collections import defaultdict
 
-if __name__ == "__main__":   
+if __name__ == "__main__":
     total_size = 0
     status_counts = defaultdict(int)
     line_count = 0
 
-    valid_status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+    valid_status_codes = {'200', '301', '400', '401',
+                          '403', '404', '405', '500'}
 
-    log_pattern = re.compile(
-        r'^(\d{1,3}(?:\.\d{1,3}){3}) - '
-        r'\[(.*?)\] "(\w+) (\S+) HTTP/1\.1" (\d{3}) (\d+)$'
-    )
-
-
-    def print_stats():
+    def print_stats(total_size, status_counts):
         print("File size: {}".format(total_size))
         for code in sorted(valid_status_codes):
             if status_counts[code] > 0:
                 print(f"{code}: {status_counts[code]}")
 
-
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = log_pattern.match(line)
-            if match:
-                status_code = match.group(5)
-                file_size = match.group(6)
-                total_size += int(file_size)
-
+            line = line.split()
+            try:
+                status_code = line[-2]
                 if status_code in valid_status_codes:
                     status_counts[status_code] += 1
+                file_size = line[-1]
+                total_size += int(file_size)
+            except Exception:
+                pass
 
             line_count += 1
             if line_count % 10 == 0:
-                print_stats()
+                print_stats(total_size, status_counts)
 
     except KeyboardInterrupt:
-        print_stats()
+        print_stats(total_size, status_counts)
         raise
-    print_stats()
+    print_stats(total_size, status_counts)
