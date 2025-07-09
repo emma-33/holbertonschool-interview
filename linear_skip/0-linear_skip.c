be8570cb11a7ab585aa75190e8f308f7d7e16e86
Emma Lateyron <emma.lateyron@orange.fr>
2025-07-09 12:06:15 +0200

    [ADD] 0-linear_skip.c

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search.h"
+
+
+/**
+ * linear_skip - Searches for a value in a skiplist
+ *
+ * @head: Pointer to the head of the skiplist to search in
+ * @value: Value to search for
+ *
+ * Return: Pointer on the first node where value is located, or NULL if not found
+ */
+skiplist_t *linear_skip(skiplist_t *head, int value)
+{
+    skiplist_t *search, *express;
+
+    if (head == NULL)
+        return (NULL);
+
+    express = head;
+    search = head;
+
+    while (express->express != NULL)
+    {
+        search = express;
+        express = express->express;
+        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+        if (express->n >= value)
+        {
+            break;
+        }
+        if (express->next == NULL)
+        {
+            search = express;
+            while (express->next != NULL)
+            {
+                express = express->next;
+            }
+            
+        }
+    }
+    printf("Value found between indexes [%lu] and [%lu]\n", search->index, express->index);
+
+    while (search->next != NULL && search->index != express->index)
+    {
+        printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
+        if (search->n == value)
+        {
+            return (search);
+        }
+        search = search->next;
+    }
+    if (search->next == NULL)
+    {
+        printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
+    }
+    if (search->n == value)
+    {
+        return (search);
+    }
+
+    return (NULL);
+}
