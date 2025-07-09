5e80d6c231680fcb699f9e0fa6d2ed7c50797d21
Emma Lateyron <emma.lateyron@orange.fr>
2025-07-09 12:18:12 +0200

    [FIX] 0-linear_skip.c // betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,56 +9,48 @@
  * @head: Pointer to the head of the skiplist to search in
  * @value: Value to search for
  *
- * Return: Pointer on the first node where value is located, or NULL if not found
+ * Return: Pointer on the first node where value is located, or NULL
  */
 skiplist_t *linear_skip(skiplist_t *head, int value)
 {
-    skiplist_t *search, *express;
+	skiplist_t *search, *express;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    express = head;
-    search = head;
+	express = head;
+	search = head;
 
-    while (express->express != NULL)
-    {
-        search = express;
-        express = express->express;
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-        if (express->n >= value)
-        {
-            break;
-        }
-        if (express->express == NULL)
-        {
-            search = express;
-            while (express->next != NULL)
-            {
-                express = express->next;
-            }
-            
-        }
-    }
-    printf("Value found between indexes [%lu] and [%lu]\n", search->index, express->index);
+	while (express->express != NULL)
+	{
+		search = express;
+		express = express->express;
+		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+		if (express->n >= value)
+			break;
+		if (express->express == NULL)
+		{
+			search = express;
+			while (express->next != NULL)
+			{
+				express = express->next;
+			}
+		}
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		search->index, express->index);
 
-    while (search->next != NULL && search->index != express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
-        if (search->n == value)
-        {
-            break;
-        }
-        search = search->next;
-    }
-    if (search->next == NULL)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
-    }
-    if (search->n == value)
-    {
-        return (search);
-    }
+	while (search->next != NULL && search->index != express->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
+		if (search->n == value)
+			break;
+		search = search->next;
+	}
+	if (search->next == NULL)
+		printf("Value checked at index [%lu] = [%d]\n", search->index, search->n);
+	if (search->n == value)
+		return (search);
 
-    return (NULL);
+	return (NULL);
 }
