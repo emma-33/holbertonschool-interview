f41df7309b3b8477dced6e683ab79ce45e6fab99
Emma Lateyron <emma.lateyron@orange.fr>
2025-07-18 14:51:44 +0200

    [ADD] 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,16 +3,19 @@ const request = require('request');
 const epNumber = process.argv[2];
 const urlPath = 'https://swapi-api.hbtn.io/api/films/' + epNumber;
 
-request(urlPath, function (err, response, body) {
+request(urlPath, async (err, response, body) => {
 	if (err) throw err;
 	const movies = JSON.parse(body);
 	const characters = movies.characters;
 
-	characters.forEach((characterUrl) => {
-	request(characterUrl, function (err, response, body) {
-	  if (err) throw err;
-	  const character = JSON.parse(body);
-	  console.log(character.name);
-	});
-  });
+	for (const characterUrl of characters) {
+		await new Promise((resolve, reject) => {
+			request(characterUrl, (err, response, body) => {
+				if (err) reject(err);
+				const character = JSON.parse(body);
+				console.log(character.name);
+				resolve();
+			});
+		});
+	}
 });
