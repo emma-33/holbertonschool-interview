06b210d083e05b3c604020d5a660b2fd9fb425b8
Emma Lateyron <emma.lateyron@orange.fr>
2025-07-18 14:55:41 +0200

    [FIX] 0-starwars_characters.js // semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,18 +4,18 @@ const epNumber = process.argv[2];
 const urlPath = 'https://swapi-api.hbtn.io/api/films/' + epNumber;
 
 request(urlPath, async (err, response, body) => {
-	if (err) throw err;
-	const movies = JSON.parse(body);
-	const characters = movies.characters;
+  if (err) throw err;
+  const movies = JSON.parse(body);
+  const characters = movies.characters;
 
-	for (const characterUrl of characters) {
-		await new Promise((resolve, reject) => {
-			request(characterUrl, (err, response, body) => {
-				if (err) reject(err);
-				const character = JSON.parse(body);
-				console.log(character.name);
-				resolve();
-			});
-		});
-	}
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterUrl, (err, response, body) => {
+        if (err) reject(err);
+        const character = JSON.parse(body);
+        console.log(character.name);
+        resolve();
+      });
+    });
+  }
 });
