82aa680316ca85a1b0f294dcea0a0f6ae72199a1
Emma Lateyron <emma.lateyron@orange.fr>
2025-08-08 11:48:16 +0200

    [FIX] 0-advanced_binary.c

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,67 +1,17 @@
-#include "search_algos.h"
 #include <stdio.h>
 #include <stddef.h>
+#include "search_algos.h"
+
 
 /**
- * advanced_binary - Searches for a value in a sorted array of integers
- * using the advanced binary search algorithm.
- * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
- * @value: The value to search for.
- * Return: The index of the first occurrence of value in the array,
- *         or -1 if value is not present or the array is NULL.
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	if (array == NULL || size == 0)
-		return (-1);
-
-	if (size == 1)
-		return (1);
-
-	int beg = 0;
-	int end = size - 1;
-
-	return (helper_binary_search(array, beg, end, value));
-}
-
-/**
- * helper_binary_search - Helps with the recursive binary search.
- * @array: Pointer to the first element of the array to search in.
- * @beg: The starting index of the current search range.
- * @end: The ending index of the current search range.
- * @value: The value to search for.
+ * print_array - Print the array or sub-array
  *
- * Return: The index of the first occurrence of value in the array,
- *         or -1 if value is not present in the array.
- */
-
-int helper_binary_search(int *array, int beg, int end, int value)
-{
-	if (end >= beg)
-	{
-		print_array(array, beg, end);
-
-		int half = beg + (end - beg) / 2;
-		if (array[half] == value && array[half - 1] != value)
-			return (half);
-
-		if (array[half] >= value)
-			return (helper_binary_search(array, beg, half - 1, value));
-
-		return (helper_binary_search(array, half + 1, end, value));
-	}
-	return (-1);
-}
-
-/**
- * print_array - Prints an array of integers.
- * @array: Pointer to the first element of the array.
- * @beg: The starting index of the array to print.
- * @end: The ending index of the array to print.
- * Return: void
+ * @array: Pointer to the first element of the array
+ * @beg: index of the first element
+ * @end: index of the last element
  */
 void print_array(int *array, int beg, int end)
+
 {
 	printf("Searching in array: ");
 	for (int i = beg; i <= end; i++)
@@ -71,4 +21,54 @@ void print_array(int *array, int beg, int end)
 			printf(", ");
 	}
 	printf("\n");
+}
+
+
+/**
+ * search - Searches for a value in a sorted array of integers
+ *
+ * @array: Pointer to the first element of the array
+ * @beg: index of the first element
+ * @end: index of the last element
+ * @value: Value to search for
+ * Return: index of the value or -1 if failed
+ */
+int search(int *array, int beg, int end, int value)
+
+{
+	if (end >= beg)
+	{
+		print_array(array, beg, end);
+		int half = beg + (end - beg) / 2;
+
+		if (array[half] == value && array[half - 1] != value)
+			return (half);
+
+		if (array[half] >= value)
+			return (search(array, beg, half, value));
+
+		return (search(array, half + 1, end, value));
+	}
+	return (-1);
+}
+
+
+/**
+ * advanced_binary - Call the function search
+ *
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in array
+ * @value: Value to search for
+ * Return: index of the value or -1 if failed
+ */
+int advanced_binary(int *array, size_t size, int value)
+
+{
+	if (array == NULL || size == 0)
+		return (-1);
+	if (size == 1)
+		return (1);
+	int beg = 0, end = size - 1;
+
+	return (search(array, beg, end, value));
 }
--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -5,7 +5,7 @@
 
 
 int advanced_binary(int *array, size_t size, int value);
-int helper_binary_search(int *array, int beg, int end, int value);
+int search(int *array, int beg, int end, int value);
 void print_array(int *array, int beg, int end);
 
 #endif /* SEARCH_ALGOS_H */
